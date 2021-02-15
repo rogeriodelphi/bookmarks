a722517ca12efb64670839445a02633414d85319
rogeriodelphi <rogeriodelphi@gmail.com>
2021-02-15 02:04:48 -0300

    Using the Django authentication

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('login/', views.user_login, name='login' ),
+    path('login/', views.user_login, name='login'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+
 from .forms import LoginForm
+
 
 def user_login(request):
     if request.method == 'POST':
@@ -9,12 +11,13 @@ def user_login(request):
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(request,
-                                user_name=cd['username'],
+                                username=cd['username'],
                                 password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('authenticate successfully')
+                    return HttpResponse('Authenticated ' \
+                                        'successfully')
                 else:
                     return HttpResponse('Disabled account')
             else:
